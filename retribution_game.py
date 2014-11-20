cd5e6bd5569a61f54b30902bf8568591390a06fc
NickJacksonpyah <s137665@gmail.com>
2014-11-20 13:52:08 -0600

    Start
    l

--- retribution_game.py
+++
@@ -1,2 +0,0 @@
-# retribution
-print "Congratulation,you are on probation period, now until you have done this task you have completed this task. You can be free whenever you have completed this task"
